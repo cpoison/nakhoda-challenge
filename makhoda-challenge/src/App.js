04be9056f73f2e41a40ce327805a60dd9ea7765b
Carlos Poison <cpoison8@gmail.com>
2019-01-06 12:37:08 +0000

    Master - Add top header bar

--- makhoda-challenge/src/App.js
+++ makhoda-challenge/src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./reset.css";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="">
+					<div className="top-bar">
+						<nav className="menu">
+							<ul>
+								<li>
+									<a href="#">Inste Natus</a>
+								</li>
+								<li>
+									<a href="#">Explicabo</a>
+								</li>
+								<li>
+									<a href="#">Omnis</a>
+								</li>
+								<li>
+									<a href="#">Ipsa quae</a>
+								</li>
+								<li>
+									<a href="#" className="selected">
+										Percitiatis Unde
+									</a>
+								</li>
+							</ul>
+						</nav>
+						<div className="user-options">
+							<button type="button" className="user-dropdown">
+								<span>Veris Veritatis</span>
+								<i class="fas fa-chevron-down" />
+							</button>
+							<button type="button" className="user-login">
+								Z
+							</button>
+						</div>
+					</div>
+					<div className="bottom-header">
+						<span className="bar-tittle">Sunt</span>
+						<div className="button-container">
+							<button type="button" className="button-icon">
+								<i class="fas fa-chevron-circle-down" />
+								<label>Text</label>
+							</button>
+						</div>
+					</div>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
